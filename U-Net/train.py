b122c7efe7cacc29225c1862e8de6d6f66b9b0c7
paulecraig79 <77742997+paulecraig79@users.noreply.github.com>
2024-03-06 11:11:50 +0000

    Optimizing imports

--- U-Net/train.py
+++ U-Net/train.py
@@ -17,7 +17,7 @@ from Models.DeepLabV3plus import DeepLab
 #Parameters
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-NUM_EPOCHS = 1
+NUM_EPOCHS = 200
 NUM_WORKERS = 2
 PIN_MEMORY = True
 LOAD_MODEL = False
